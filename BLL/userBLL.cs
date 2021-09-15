4204ddff43eb6d1024a83839d7757fe3f68e3b3d
sithuvindy <sithuvindy@gmail.com>
2021-09-15 16:53:56 +0530

    Removed the user BLL comment

--- BLL/userBLL.cs
+++ BLL/userBLL.cs
@@ -8,7 +8,7 @@ namespace AnyStore.BLL
 {
     class userBLL
     {
-        //Done 
+        
         public int id { get; set; }
         public string firstname { get; set; }
         public string lastname { get; set; }
